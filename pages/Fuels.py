b185fd0790b0a1b30fbc2e43fbaf7337fe7a92e2
Rajratan Gulab More <145323630+rajratangm@users.noreply.github.com>
2024-11-05 17:05:57 +0530

    Update Fuels.py

--- pages/Fuels.py
+++ pages/Fuels.py
@@ -6,7 +6,7 @@ col1, col2, col3 = st.columns(3)
 selected = option_menu(
     menu_title=None,
     icons=['k','d','l'],
-    options=['Home','Projects','Contact','',], 
+    options=['Home','Projects',''], 
     orientation='horizontal',
     styles={
         "container":{'background-color':'white','display':'flex','justify-content':'flex-start','weight':'100px'},
