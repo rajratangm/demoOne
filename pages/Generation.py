bb8138c8cd286f9b0b4b8aaee4b92773a9fcbea6
Rajratan Gulab More <145323630+rajratangm@users.noreply.github.com>
2024-11-05 17:09:28 +0530

    Update Generation.py

--- pages/Generation.py
+++ pages/Generation.py
@@ -1,50 +1,26 @@
-import streamlit as st
+from streamlit_option_menu import option_menu
+import streamlit as st 
 
-# Set the page configuration
-st.set_page_config(page_icon="🏭")
 
-# Set the header
-st.header('Generation')
 
-# Initialize session state for selected option
-if 'selected' not in st.session_state:
-    st.session_state.selected = 'Generation by Unit'  # Set a default option
+selected = option_menu(
+    menu_title=None,
+    icons=['k','d','l','kp'],
+    options=['Generation Option1','Generation Option2','',''], 
+    orientation='horizontal',
+    styles={
+        "container":{'background-color':'white','display':'flex','justify-content':'flex-start','weight':'100px'},
+        "text":{'color':'black'},
+        'nav-link':{'border-bottom':'3px solid lightgray', 'color':'black', 'border-radius':'0px'},
+        'nav-link-selected':{'border-bottom':'3px solid red', 'background-color':'white',},
+        'nav-link-contact':{'margin-right':'auto','padding-rght':'50px'}
+        
 
-# Create buttons for navigation
-# button_style = """
-# <style>
-#     .stButton > button {
-#         font-size: 10px;
-#         text-align: left;
-#         margin-left: 0!important;
-#         text-decoration: none;
-#         border: none;  /* Remove default button border */
-#         color: black;
-#         padding: 5px 10px;  /* Adjust padding for closer spacing */
-#         border-bottom: 3px solid grey;  /* Default grey underline */
-#         border-radius: 0px;
-#         background-color: transparent;  /* Transparent background */
-#         width: 100%;  /* Full width for buttons */
-#     }
-#     .stButton > button:hover {
-#         color: red;
-#         border-bottom: 3px solid red;  /* Red underline on hover */
-#     }
-#     .stButton > button:focus {
-#         outline: none;  /* Remove focus outline */
-#     }
-# </style>
-# """
-# st.markdown(button_style, unsafe_allow_html=True)
+    }
+)
 
-# Define button click behavior
-if st.button("Generation by Unit"):
-    st.session_state.selected = "Generation by Unit"
-elif st.button("Generation by Fuel"):
-    st.session_state.selected = "Generation by Fuel"
-
-# Display content based on selection
-if st.session_state.selected == "Generation by Unit":
-    st.write("Displaying data for Generation by Unit...")
-elif st.session_state.selected == "Generation by Fuel":
-    st.write("Displaying data for Generation by Fuel...")
+if selected=='Fuel Option1':
+    st.title('You have selected Fuel Option1 ')
+    st.checkbox('Show all options')
+if selected=='Fuel Option2':
+    st.title('You have selected Fuel Option2')
