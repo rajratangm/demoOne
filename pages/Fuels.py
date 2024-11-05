fb79e3db88dc151353380231665ba80ea2fa23a3
Rajratan Gulab More <145323630+rajratangm@users.noreply.github.com>
2024-11-05 17:08:13 +0530

    Update Fuels.py

--- pages/Fuels.py
+++ pages/Fuels.py
@@ -6,22 +6,21 @@ col1, col2, col3 = st.columns(3)
 selected = option_menu(
     menu_title=None,
     icons=['k','d','l','kp'],
-    options=['Home','Projects','',''], 
+    options=['Fuel Option1','Fuel Option2','',''], 
     orientation='horizontal',
     styles={
         "container":{'background-color':'white','display':'flex','justify-content':'flex-start','weight':'100px'},
         "text":{'color':'black'},
         'nav-link':{'border-bottom':'3px solid lightgray', 'color':'black', 'border-radius':'0px'},
-        'nav-link-selected':{'border-bottom':'3px solid red', 'background-color':'white'},
+        'nav-link-selected':{'border-bottom':'3px solid red', 'background-color':'white',},
         'nav-link-contact':{'margin-right':'auto','padding-rght':'50px'}
         
 
     }
 )
 
-if selected=='Home':
-    st.title('You have selected Home ')
-if selected=='Projects':
-    st.title('You have selected Projects')
-if selected =='Contact':
-    st.title('you have selected contacts')
+if selected=='Fuel Option1':
+    st.title('You have selected Fuel Option1 ')
+if selected=='Fuel Option2':
+    st.title('You have selected Fuel Option2')
+
