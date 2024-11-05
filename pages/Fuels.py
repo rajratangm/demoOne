855263c5489c1a3ba83204d90e41f203f38e0392
Rajratan Gulab More <145323630+rajratangm@users.noreply.github.com>
2024-11-05 17:05:26 +0530

    Update Fuels.py

--- pages/Fuels.py
+++ pages/Fuels.py
@@ -2,18 +2,18 @@ from streamlit_option_menu import option_menu
 import streamlit as st 
 
 
-
+col1, col2, col3 = st.columns(3)
 selected = option_menu(
     menu_title=None,
-    icons=['k','d','l','p','o'],
-    options=['Fuel Option1','Fuel Option2','','',''], 
+    icons=['k','d','l'],
+    options=['Home','Projects','Contact','',], 
     orientation='horizontal',
     styles={
-        "container":{'background-color':'white'},
+        "container":{'background-color':'white','display':'flex','justify-content':'flex-start','weight':'100px'},
         "text":{'color':'black'},
-        'nav-link':{'border-bottom':'3px solid lightgray', 'color':'black', 'border-radius':'0px','text-size':'10px'},
-        'nav-link-selected':{'border-bottom':'3px solid red', 'background-color':'white','text-size':'10px'},
-        
+        'nav-link':{'border-bottom':'3px solid lightgray', 'color':'black', 'border-radius':'0px'},
+        'nav-link-selected':{'border-bottom':'3px solid red', 'background-color':'white'},
+        'nav-link-contact':{'margin-right':'auto','padding-rght':'50px'}
         
 
     }
