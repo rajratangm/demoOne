26dee5210e7fe2fbe6e35ddb5cde4e8ba74f8fab
Rajratan Gulab More <145323630+rajratangm@users.noreply.github.com>
2024-11-05 17:09:54 +0530

    Update Generation.py

--- pages/Generation.py
+++ pages/Generation.py
@@ -2,25 +2,26 @@ from streamlit_option_menu import option_menu
 import streamlit as st 
 
 
-
+col1, col2, col3 = st.columns(3)
 selected = option_menu(
     menu_title=None,
-    icons=['k','d','l','kp'],
-    options=['Generation Option1','Generation Option2','',''], 
+    icons=['k','d','l'],
+    options=['Home','Projects','Contact','',], 
     orientation='horizontal',
     styles={
         "container":{'background-color':'white','display':'flex','justify-content':'flex-start','weight':'100px'},
         "text":{'color':'black'},
         'nav-link':{'border-bottom':'3px solid lightgray', 'color':'black', 'border-radius':'0px'},
-        'nav-link-selected':{'border-bottom':'3px solid red', 'background-color':'white',},
+        'nav-link-selected':{'border-bottom':'3px solid red', 'background-color':'white'},
         'nav-link-contact':{'margin-right':'auto','padding-rght':'50px'}
         
 
     }
 )
 
-if selected=='Fuel Option1':
-    st.title('You have selected Fuel Option1 ')
-    st.checkbox('Show all options')
-if selected=='Fuel Option2':
-    st.title('You have selected Fuel Option2')
+if selected=='Home':
+    st.title('You have selected Home ')
+if selected=='Projects':
+    st.title('You have selected Projects')
+if selected =='Contact':
+    st.title('you have selected contacts')
