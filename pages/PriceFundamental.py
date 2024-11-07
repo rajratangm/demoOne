aa018b4e09ec62cf0e8b28e97d493702f5b1ad1a
Rajratan Gulab More <145323630+rajratangm@users.noreply.github.com>
2024-11-07 16:25:25 +0530

    Update PriceFundamental.py

--- pages/PriceFundamental.py
+++ pages/PriceFundamental.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import pandas as pd 
 import matplotlib.pyplot as plt
 
-st.set_page_config(page_icon="🗾")
+st.set_page_config(page_icon="🗾", page_title='PriceFundamental')
 
 st.header('Price and Fundamentals')
 
