1e9fe8f468ab956a4759cb70685f8562382fe834
Rajratan Gulab More <145323630+rajratangm@users.noreply.github.com>
2024-11-11 09:51:19 +0530

    Update Flows.py

--- pages/Flows.py
+++ pages/Flows.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import pandas as pd 
 import matplotlib.pyplot as plt
 
-st.set_page_config(page_icon="↔️", page_title='Flows")
+st.set_page_config(page_icon="↔️", page_title="Flows")
 st.header('Price and Fundamentals')
 
 with st.sidebar:
